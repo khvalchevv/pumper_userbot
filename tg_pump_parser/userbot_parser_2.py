7aec69bf4fdae4b317436c89f77c700cd883097c
khvalchevv <vladik25032010@gmail.com>
2025-06-30 00:58:05 +0300

    Update userbot_parser_2.py

--- tg_pump_parser/userbot_parser_2.py
+++ tg_pump_parser/userbot_parser_2.py
@@ -14,7 +14,7 @@ TARGET_CHAT_ID_2 = -1002604238211
 TARGET_THREAD_ID_2 = 2899
 
 SELECTED_TOKENS = [
-    "STMX", "PAWS", "IDOL", "ELDE", "SPK", "EDGEN", "PRAI", "WLD", "BLUE", "BOND",
+    "STMX", "PAWS", "IDOL", "ELDE", "SPK", "EDGEN", "PRAI", "WLD", "BLUE",
     "WHY", "ORDER", "FLY", "KEY", "LUCE", "ZCX", "KILO", "BID", "SHM", "BLZ", "OMG",
     "AMB", "IMT", "PFWS"
 ]
