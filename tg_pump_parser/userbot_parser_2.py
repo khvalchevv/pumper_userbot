0244be50f579a72809dae2a2ab632966e7894d9f
khvalchevv <vladik25032010@gmail.com>
2025-07-03 18:25:53 +0300

    Update userbot_parser_2.py

--- tg_pump_parser/userbot_parser_2.py
+++ tg_pump_parser/userbot_parser_2.py
@@ -15,7 +15,7 @@ TARGET_THREAD_ID_2 = 2899
 
 SELECTED_TOKENS = ["IDOL", "ELDE", "SPK", "EDGEN", "PRAI", "WLD", "BLUE",
     "WHY", "ORDER", "FLY", "KEY", "LUCE", "ZCX", "KILO", "BID", "SHM", "BLZ", "OMG",
-    "AMB", "IMT", "PFWS", "broccoli"
+    "AMB", "PFWS", "broccoli", "DBR", "GHIBLI", "EDGE", ""
 ]
 
 TOKENS_TRACK_FILE = "forwarded_tokens_gate.json"
